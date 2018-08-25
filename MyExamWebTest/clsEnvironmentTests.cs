315caeee3c9b4620fdcf6782acd4fea4943ba6c4
Tanameck <Tanameck@gmail.com>
2018-08-24 23:00:44 -0600

    unit test cases. new project asp net core

---
+++ MyExamWebTest/clsEnvironmentTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyExamWebTest
+{
+    [TestClass]
+    class clsEnvironmentTests
+    {
+        [TestMethod]
+        public void pfnConnect_SuccessfulConnection_ReturnTrue()
+        {
+            var toEnvironment = new clsEnvironment();
+
+            
+        }
+    }
+}
---
+++ MyExamWebTest/clsUserTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyExamWebTest
+{
+    [TestClass]
+    public class clsUserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
